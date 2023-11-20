90aaf40dc3f9cfc58952b82f933d0d02ee2449a1
Lucas Lima <lucasViniLima15@gmail.com>
2023-11-20 11:34:52 -0300

    customize the details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import {HomeComponent} from './home/home.component';
-import {RouterModule} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   template: `
     <main>
       <a [routerLink]="['/']">
